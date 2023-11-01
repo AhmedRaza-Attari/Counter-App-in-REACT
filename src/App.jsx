a424d577c6c42f3c05bf795e1fa9384563287fbd
AhmedRaza-Attari <ahmedraza8326@gmail.com>
2023-11-01 19:40:12 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,33 @@ function App() {
 
   const [counter, setCounter] = useState(0);
 
-  const incrementValue = () => setCounter(counter + 1);
+  // const incrementValue = () => setCounter(counter + 1);
+
+  // const incrementValue = () => {
+  //   setCounter(counter + 1);
+  //   setCounter(counter + 1);
+  //   setCounter(counter + 1);
+  //   setCounter(counter + 1);
+  // }
+
+  // Below the code works because 'setCounter' by default gives us the previous state, and the above code can't works because the code is same and that's why the code is update in a combine called 'batch file'
+  const incrementValue = () => {
+    // setCounter(() => {})
+    setCounter(preCounter => preCounter + 1);
+    setCounter(preCounter => preCounter + 1);
+    setCounter(preCounter => preCounter + 1);
+    setCounter(preCounter => preCounter + 1);
+  }
+
   const decrementValue = () => {
     if (counter > 0) setCounter(counter - 1)
   } 
-
+ 
   return (
     <>
       <h1> Couter App </h1>
 
-      <h1> Counter Value : {counter} </h1>
+      <h2> Counter Value : {counter} </h2>
 
       <button onClick={incrementValue}> Increment </button> <br />
       <button onClick={decrementValue}> Decrement </button>
